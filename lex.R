3359fa064fdf4a8d530b690d7634760c0c8c0864
dmpe <cincenko@seznam.cz>
2015-12-28 18:07:33 +0100

    add some lex. features

--- lex.R
+++
@@ -1,9 +0,0 @@
-library(openNLP)
-library(NLP)
-library(rJava)
-library(RWeka)
-library(stringi)
-library(readr)
-
-data_n <-  readr::read_csv("~/Documents/python-notebook/data_utf8.csv")
-data_n$StringLength <- stri_length(data_n$STATUS)
