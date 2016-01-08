51bff53267545d84b00fdb877e94e98e7c5acc00
dmpe <cincenko@seznam.cz>
2016-01-08 16:38:17 +0100

    again, trying to work on feature union

--- R_scripts_and_notebooks/lexical_features.R
+++ R_scripts_and_notebooks/lexical_features.R
@@ -3,8 +3,8 @@
 # library(openNLP)
 # library(NLP)
 # library(RWeka)
-library(quanteda)
 library(stringi)
+library(quanteda)
 library(readr)
 
 data_n <- readr::read_csv("~/Documents/python-notebook/raw_data/data_utf8.csv")
@@ -17,7 +17,7 @@ for (i in 1:length(data_n$STATUS)) {
   data_n$Number_of_Semicolons[[i]] <- stri_count_fixed(data_n$STATUS[i], ";")
   data_n$Number_of_Colons[[i]] <- stri_count_fixed(data_n$STATUS[i], ":")
   data_n$Avarage_Word_Lenght[[i]] <- round((data.frame(t(stri_stats_latex(data_n$STATUS[i])))$CharsWord / data_n$Number_of_Words[[i]]), 3)
-  data_n$Lexical_Diversity[[i]] <- round(quanteda::lexdiv(dfm(data_n$STATUS[i], verbose = F, removeNumbers = F, removePunct = F, removeSeparators = F), "TTR"), 3)
+  data_n$Lexical_Diversity[[i]] <- round(quanteda::lexdiv(quanteda::dfm(data_n$STATUS[i], verbose = F, removeNumbers = F, removePunct = F, removeSeparators = F), measure = "TTR"), 3)
 }
 data_n$Avarage_Word_Lenght[is.nan(data_n$Avarage_Word_Lenght)] <- 0
 
