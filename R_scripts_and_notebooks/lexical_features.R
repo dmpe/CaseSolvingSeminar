2eab26917965b4ea06871f5da098e45f606fc67b
dmpe <cincenko@seznam.cz>
2016-01-10 19:40:28 +0100

    update and fix R script

--- R_scripts_and_notebooks/lexical_features.R
+++ R_scripts_and_notebooks/lexical_features.R
@@ -1,11 +1,19 @@
 # https://github.com/Rexamine/stringi/issues/212
 
-# library(openNLP)
-# library(NLP)
-# library(RWeka)
+library(openNLP)
+library(NLP)
+library(RWeka)
+library("RWekajars")
 library(stringi)
 library(quanteda)
+library(SnowballC)
+library(methods)
 library(readr)
+library(Rcpp)
+library(utils)
+library(stats)
+library(MASS)
+library(wordcloud)
 
 data_n <- readr::read_csv("~/Documents/python-notebook/raw_data/data_utf8.csv")
 data_n$StringLength <- stri_length(data_n$STATUS)
@@ -17,8 +25,13 @@ for (i in 1:length(data_n$STATUS)) {
   data_n$Number_of_Semicolons[[i]] <- stri_count_fixed(data_n$STATUS[i], ";")
   data_n$Number_of_Colons[[i]] <- stri_count_fixed(data_n$STATUS[i], ":")
   data_n$Avarage_Word_Lenght[[i]] <- round((data.frame(t(stri_stats_latex(data_n$STATUS[i])))$CharsWord / data_n$Number_of_Words[[i]]), 3)
-  data_n$Lexical_Diversity[[i]] <- round(quanteda::lexdiv(quanteda::dfm(data_n$STATUS[i], verbose = F, removeNumbers = F, removePunct = F, removeSeparators = F), measure = "TTR"), 3)
 }
+
+for (k in 1:length(data_n$STATUS)) {
+  dfmStatus <- dfm(data_n$STATUS[k], verbose = F, removeNumbers = F, removePunct = F, removeSeparators = F)
+  data_n$Lexical_Diversity[[k]] <- round(quanteda::lexdiv(dfmStatus, measure = "TTR"), 3)
+}
+
 data_n$Avarage_Word_Lenght[is.nan(data_n$Avarage_Word_Lenght)] <- 0
 
 colnames(data_n)[1] <- "#AUTHID" ## WTF?
