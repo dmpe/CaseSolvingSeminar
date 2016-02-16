b3922bab41978da7b0a5ffc8fd2955c656e31c5d
Dustin Wind <dustywind@users.noreply.github.com>
2016-02-16 12:59:29 +0100

    CharacterFreatures will now return a manipulated copy of its input

--- src/cssfeature/character.py
+++ src/cssfeature/character.py
@@ -1,12 +1,22 @@
 
+import pandas
 
 class CharacterFeatures(object):
 
     @staticmethod
-    def string_length(data, column):
-        y = data.apply(CharacterFeatures.sentence_length, axis=1, raw=True, args=(column,))
-        y.columns = ['len(sentence)' if c == column else s for c,s in enumerate(y.columns)]
-        return y
+    def string_length(data, column, column_name='len(sentence)', index=True):
+        cpy = data.copy()
+        n_col_names = [column_name if c == column else s for c,s in enumerate(cpy.columns)]
+        cpy  = cpy.apply(
+            CharacterFeatures.sentence_length
+            , axis='columns'
+            , raw=True
+            , args=(column,)
+        )
+        cpy.columns = n_col_names
+        if index:
+            cpy.set_index(n_col_names)
+        return cpy
 
     @staticmethod
     def sentence_length(a, index):
