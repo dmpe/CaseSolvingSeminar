8bbea69ff0c471ce9782a45eb128636525a574b7
Dustin Wind <dustywind@users.noreply.github.com>
2016-02-22 15:24:23 +0100

    added reader for aggregated s* labels

--- src/csshelper/__init__.py
+++ src/csshelper/__init__.py
@@ -1,6 +1,8 @@
 
 from .datareader import DataReader
 
+from .aggregated_reader import AggregatedReader
+
 from .testreader import TestReader
 
 from .cext_reader import CExtReader
@@ -17,4 +19,16 @@ from .scon_reader import SConReader
 
 from .copn_reader import COpnReader
 from .sopn_reader import SOpnReader
+
+from .cagr_aggregated_reader import CAgrAggregatedReader
+from .ccon_aggregated_reader import CConAggregatedReader
+from .cext_aggregated_reader import CExtAggregatedReader
+from .cneu_aggregated_reader import CNeuAggregatedReader
+from .copn_aggregated_reader import COpnAggregatedReader
+
+from .sagr_aggregated_reader import SAgrAggregatedReader
+from .scon_aggregated_reader import SConAggregatedReader
+from .sext_aggregated_reader import SExtAggregatedReader
+from .sneu_aggregated_reader import SNeuAggregatedReader
+from .sopn_aggregated_reader import SOpnAggregatedReader
 
---
+++ src/csshelper/sext_aggregated_reader.py
@@ -0,0 +1,15 @@
+
+from . import AggregatedReader
+
+class SExtAggregatedReader(AggregatedReader):
+    
+    def __init__(self, sqlite_connection, determine_value=lambda x: int(x)):
+        column = 'sExt'
+        super(SAgrReader, self).__init__(sqlite_connection, column)
+        self.determine_value = determine_value
+        pass
+
+    def _result_converter(self, result):
+        return str(result[0]), self.determine_value(result[1])
+
+
